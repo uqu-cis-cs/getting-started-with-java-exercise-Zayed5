1318280af094fe9c409e25541783725bee19c760
Zayed5 <112311123+Zayed5@users.noreply.github.com>
2022-09-13 10:30:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    System.out.print("enter your name : ");
+    String inp1 = inp.nextLine();
+    System.out.println("welcom " + inp1);
+  }
+}
